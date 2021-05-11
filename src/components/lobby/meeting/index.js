a9a170e460f8ddac924d74fce7d1a19dab55c60d
Ayushman Tripathy <54672392+ayushmantripathy9@users.noreply.github.com>
2021-05-11 12:08:33 +0530

    💯 Self video and npm packages fix (#12)
    * :construction: Self-video for WebRTC
    
    * :bug: npm packages fix

--- src/components/lobby/meeting/index.js
+++ src/components/lobby/meeting/index.js
@@ -1,18 +1,109 @@
-import {Component} from 'react'
+import React, { Component } from 'react';
+
 import './css/index.css'
 
-class Meeting extends Component {
+import { Button, Icon } from 'semantic-ui-react'
 
-    render () {
-        return (
-            <div
-                id='meeting-container'
-            >
 
+
+class Meeting extends Component{
+
+    constructor(props){
+        super(props)
+        this.state = {
+            primaryVideo: null,
+            partnerVideo: null,
+            audioOn: false,
+            videoOn: false,
+        }
+        this.primaryVideoRef = React.createRef()
+        this.secondaryVideoRef = React.createRef()
+        this.control_buttons = [
+            {
+                key: "microphone",
+                icon: "microphone",
+                action: this.myAudio
+            },
+            {
+                key: "video",
+                icon: "video",
+                action: this.myVideo
+            },
+        ]
+    }
+
+    componentDidMount(){
+
+    }
+    
+
+    myAudio () {
+
+    }
+
+    myVideo () {
+        if(!this.state.videoOn){
+            navigator.mediaDevices.getUserMedia({video: { width: 1200, height: 680 }, audio:false}).then(
+                stream => {
+                    if(this.primaryVideoRef.current){
+                        this.primaryVideoRef.current.srcObject = stream
+                    }
+                    this.setState({
+                        videoOn: true
+                    })
+                },
+                
+                
+            ).catch(err=>alert("Holaaaaa",err))
+        }
+        else{
+            this.setState({
+                videoOn: false
+            })
+            this.primaryVideoRef.current.srcObject.getTracks().forEach(element => {
+                element.stop()
+            }); 
+        }      
+        
+    }
+    
+
+    render(){
+        const {primaryVideo, partnerVideo, videoOn, audioOn, screenShare} = this.state
+        const conditions = [ audioOn, videoOn, screenShare ]
+        
+        return(
+            <div id='meeting-container'>
+                <div id="primaryVideoContainer">
+                    <video id="primaryVideo" ref={this.primaryVideoRef} autoPlay/>
+                </div>
+                <div id='meeting-content'>
+                    <div id="controlsContainer">
+                        {
+                            this.control_buttons.map((c, index) => {
+                                return (
+                                    <Button
+                                        onClick={c.action.bind(this)}
+                                        icon
+                                        color='black'
+                                    >
+                                        <Icon 
+                                            name={c.icon}
+                                            color={conditions[index]?"green":"red"}
+                                        />
+                                    </Button>
+                                )
+                            })
+                        }
+                    </div>
+                    <div id='secondaryVideoContainer'>
+                        <video id='secondaryVideo' ref={this.secondaryVideoRef} autoPlay />
+                    </div>
+                </div>
+    
             </div>
         )
     }
 }
-
 
 export default Meeting
